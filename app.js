311f34709d5e2c8862f3a718450dd7b42884a082
shaidavis <shaidavis@gmail.com>
2016-09-06 09:51:46 +0300

    first commit

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+console.log("*****SERVER IS RUNNING*****")
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+app.use(express.static('models'));
 
 app.use('/', routes);
 app.use('/users', users); 
